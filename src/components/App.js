6ab67738bea044b40cd1188f6a489cf49142142c
Anna shabbagh <afreenshabbagh@gmail.com>
2025-01-22 16:28:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,39 @@
-import React, { useState, useEffect } from "react";
-// Removed the import of 'App.css' since it seems missing
-import WeatherDisplay from './WeatherDisplay';
+
+import React, { useState,useEffect } from "react";
+import './../styles/App.css';
+import WeatherDisplay from "./WeatherDisplay";
 
 const App = () => {
-  const [weatherData, setWeatherData] = useState({ temperature: 25, conditions: "Sunny" });
+  const [weatherData,setWeatherData]=useState(
+    {temperature: 0, conditions: "" }
+  )
 
   useEffect(() => {
-    setWeatherData({ temperature: 25, conditions: "Sunny" });
+    // let pas = "931d9a0688d73e3af0451ed87dcfe233";
+    // let fetc = `https://api.openweathermap.org/data/2.5/weather?q=${"patna"}&units=metric&appid=${pas}`;
+    // fetch(fetc)
+    //   .then((data) => {
+    //     return data.json();
+    //   })
+    //   .then((data1) => {
+    //     console.log(data1);
+    //     setWeatherData([
+    //       { temperature: data1.main.temp, conditions: data1.weather[0].description },
+    //     ]);
+    //   })
+    //   .catch((error) => {
+    //     alert("sorry data not found");
+    //   });
+     setWeatherData({temperature: 25, conditions: "Sunny" });
   }, []);
 
+  
   return (
     <div>
-      <WeatherDisplay weather={weatherData} />
+      
+         <WeatherDisplay weatherData={weatherData}/>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
